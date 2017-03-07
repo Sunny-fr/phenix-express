181e61942e592b2e6add1e0e749e2e8d187aa742
Alaa El Helw <work@sunny.fr>
2017-03-07 10:43:56 +0100

    build

--- index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    server: require('./src/server'),
-    Collection: require('./src/framework/collection/Collection'),
-    MariaCollection: require('./src/framework/collection/MariaCollection'),
-    Model: require('./src/framework/model/Model'),
-    MariaModel: require('./src/framework/model/MariaModel'),
-    MariaQueryBuilder: require('./src/framework/query/MariaQueryBuilder')
-}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    server: require('./server'),
+    Collection: require('./framework/collection/Collection'),
+    MariaCollection: require('./framework/collection/MariaCollection'),
+    Model: require('./framework/model/Model'),
+    MariaModel: require('./framework/model/MariaModel'),
+    MariaQueryBuilder: require('./framework/query/MariaQueryBuilder')
+}
